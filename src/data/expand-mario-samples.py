cfd224d502dfd17e5ac4749f92785a3e8949af76
Victor Le Roy <victorlrmatos@gmail.com>
2024-07-25 23:37:22 -0300

    fix: change room to level at exapnd-mario-samples

--- src/data/expand-mario-samples.py
+++ src/data/expand-mario-samples.py
@@ -6,12 +6,12 @@ mario_samples = os.listdir(samples_dir)
 room_size = (28, 14)
 expand_to = (32, 32)
 
-def save_room_file(room, room_num):
+def save_room_file(level, level_num):
   save_dir = os.path.join(os.getcwd(), 'mario-samples', 'expanded-samples')
   if not os.path.exists(save_dir):
     os.makedirs(save_dir)
-  with open(os.path.join(save_dir, f'room_{room_num}.txt'), 'w') as f:
-    for row in room:
+  with open(os.path.join(save_dir, f'level_{level_num}.txt'), 'w') as f:
+    for row in level:
       f.write(''.join(row) + '\n')
 
 for i, room in enumerate(mario_samples):
