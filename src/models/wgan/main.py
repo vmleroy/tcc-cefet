05653e45004a2672674920cf171770556fd2dd5e
Victor Le Roy <victorlrmatos@gmail.com>
2024-08-14 22:48:19 -0300

    feat: create combined network

--- src/models/wgan/main.py
+++ src/models/wgan/main.py
@@ -4,6 +4,7 @@ import os
 
 from critic import WganCritic
 from generator import WganGenerator
+from wgan import Wgan
 from utils import *
 
 latent_dim = 5 # 10 for zelda, 5 for mario
@@ -15,6 +16,7 @@ n_steps_critic = 100
 
 critic = WganCritic(input_shape=(32, 32, n_blocks), batch_size=batch_size)
 generator = WganGenerator(output_shape=(32, 32, n_blocks), latent_dim=latent_dim, batch_size=batch_size)
+wgan = Wgan(generator, critic)
 
 # Y = np.ones((batch_size, 1))
 # X = np.random.randn(batch_size * latent_dim)
@@ -31,25 +33,24 @@ print('X_real', X_real.shape, 'Y_real', Y_real.shape)
 X_fake, Y_fake = generate_fake_samples(generator, latent_dim, batch_size)
 print('X_fake', X_fake.shape, 'Y_fake', Y_fake.shape)
 
-critic_loss, critic_acc = list(), list()
-generator_loss = list()
+critic_loss_hist_1, critic_loss_hist_2 = list(), list()
+generator_loss_hist = list()
 for i in range(n_steps):
-  c_loss_temp, c_acc_temp = list(), list()
+  # Update the critic more than the generator
+  c_loss_1_temp, c_loss_2_temp = list(), list()
   for _ in range(n_steps_critic):
     X_real, Y_real = generate_real_samples(dataset, batch_size)
     X_fake, Y_fake = generate_fake_samples(generator, latent_dim, batch_size)
-    c_loss1, c_acc1 = critic.model.train_on_batch(X_real, Y_real)
-    c_loss2, c_acc2 = critic.model.train_on_batch(X_fake, Y_fake)
-    c_loss_temp.append(c_loss1)
-    c_loss_temp.append(c_loss2)
-    c_acc_temp.append(c_acc1)
-    c_acc_temp.append(c_acc2)
-  critic_loss.append(np.mean(c_loss_temp))
-  critic_acc.append(np.mean(c_acc_temp))
+    c_loss_1 = critic.model.train_on_batch(X_real, Y_real)
+    c_loss_2 = critic.model.train_on_batch(X_fake, Y_fake)
+    c_loss_1_temp.append(c_loss_1)
+    c_loss_2_temp.append(c_loss_2)
+    
+  critic_loss_hist_1.append(np.mean(c_loss_1_temp))
+  critic_loss_hist_2.append(np.mean(c_loss_2_temp))  
   X_gan = generate_latent_points(latent_dim, batch_size)
   Y_gan = -np.ones((batch_size, 1))
-  # g_loss = gan.train_on_batch(X_gan, Y_gan)
-  # generator_loss.append(g_loss)
-  # generator_accuracy.append(g_loss)
-
-  # print(f'Epoch: {i}, Critic Loss: {d_loss}, Generator Loss: {g_loss}')
+  g_loss = wgan.model.train_on_batch(X_gan, Y_gan)
+  generator_loss_hist.append(g_loss)
+  if i % 50 == 0:
+    print(f'Epoch {i+1}, Critic Loss: {critic_loss_hist_1[-1]}, {critic_loss_hist_2[-1]}, Generator Loss: {generator_loss_hist[-1]}')
--- src/models/wgan/wgan.py
+++ src/models/wgan/wgan.py
@@ -1,4 +1,14 @@
+import keras as k
+
+from utils import wasserstein_loss
+
 class Wgan:
   def __init__(self, generator, critic):
     self.generator = generator
     self.critic = critic
+    self.critic.model.trainable = False
+    
+    self.model = k.models.Sequential([self.generator.model, self.critic.model])
+    
+    optimizer = k.optimizers.RMSprop(learning_rate=0.00005)
+    self.model.compile(loss=wasserstein_loss, optimizer=optimizer)
