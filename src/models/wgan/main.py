7e19adce77e912cd1fc4ca1d203c1d841650cc12
Victor Le Roy <victorlrmatos@gmail.com>
2024-08-14 22:00:52 -0300

    feat: add wgan train code todo: create combination of networks and organize code

--- src/models/wgan/main.py
+++ src/models/wgan/main.py
@@ -6,7 +6,7 @@ from critic import WganCritic
 from generator import WganGenerator
 from utils import *
 
-latent_dim = 10 # 10 for zelda, 5 for mario
+latent_dim = 5 # 10 for zelda, 5 for mario
 n_blocks = 10 # 3 for zelda, 10 for mario
 
 batch_size = 16
@@ -28,24 +28,26 @@ labels = label_encoding(labels)
 num_batches = dataset.shape[0] / batch_size
 X_real, Y_real = generate_real_samples(dataset, batch_size)
 print('X_real', X_real.shape, 'Y_real', Y_real.shape)
-print('X_real_example', X_real[0])
 X_fake, Y_fake = generate_fake_samples(generator, latent_dim, batch_size)
 print('X_fake', X_fake.shape, 'Y_fake', Y_fake.shape)
 
-critic_loss, critic_accuracy = list(), list()
-generator_loss, generator_accuracy = list(), list()
+critic_loss, critic_acc = list(), list()
+generator_loss = list()
 for i in range(n_steps):
+  c_loss_temp, c_acc_temp = list(), list()
   for _ in range(n_steps_critic):
     X_real, Y_real = generate_real_samples(dataset, batch_size)
     X_fake, Y_fake = generate_fake_samples(generator, latent_dim, batch_size)
-    d_loss1 = critic.model.train_on_batch(X_real, Y_real)
-    d_loss2 = critic.model.train_on_batch(X_fake, Y_fake)
-    d_loss = 0.5 * np.add(d_loss1, d_loss2)
-    critic_loss.append(d_loss)
-    critic_accuracy.append(0.5 * np.add(d_loss1, d_loss2))
-
-  # X_gan = generate_latent_points(latent_dim, batch_size)
-  # Y_gan = -np.ones((batch_size, 1))
+    c_loss1, c_acc1 = critic.model.train_on_batch(X_real, Y_real)
+    c_loss2, c_acc2 = critic.model.train_on_batch(X_fake, Y_fake)
+    c_loss_temp.append(c_loss1)
+    c_loss_temp.append(c_loss2)
+    c_acc_temp.append(c_acc1)
+    c_acc_temp.append(c_acc2)
+  critic_loss.append(np.mean(c_loss_temp))
+  critic_acc.append(np.mean(c_acc_temp))
+  X_gan = generate_latent_points(latent_dim, batch_size)
+  Y_gan = -np.ones((batch_size, 1))
   # g_loss = gan.train_on_batch(X_gan, Y_gan)
   # generator_loss.append(g_loss)
   # generator_accuracy.append(g_loss)
