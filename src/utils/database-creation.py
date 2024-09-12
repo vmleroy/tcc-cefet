be7dcce9443f1e07338c77d87b155132aaf1ee53
Victor Le Roy <victorlrmatos@gmail.com>
2024-09-12 18:44:41 -0300

    feat: finish zelda samples

--- src/utils/database-creation.py
+++ src/utils/database-creation.py
@@ -1,9 +1,21 @@
 import os
 import argparse
+import json
 
 parser = argparse.ArgumentParser(description='Create a database from a directory of samples')
-parser.add_argument('--game', help='The game to create a database for', choices=['zelda', 'mario'])
+parser.add_argument('--game', help='The game to create a database for', choices=['zelda', 'mario'], default='mario')
 opt = parser.parse_args()
+
+##
+# MARIO HELP FUNCTIONS
+##
+def generate_mario_samples(files):
+  width = 28
+  height = 14
+  
+  samples = []
+  samples_count = 0
+  
 
 ###
 # ZELDA HELP FUNCTIONS
@@ -12,13 +24,15 @@ def generate_zelda_samples(files):
   width = 16
   height = 11
   
+  samples = []
+  samples_count = 0
+  
   for file in files:
     with open(file, 'r') as f:
       print(f'Processing {file}...')
       zelda_map = f.read().splitlines()
       
       print(f'File rows: {len(zelda_map)}, File columns: {len(zelda_map[0])}')
-      file_count = 0
       
       rooms_per_width = int(len(zelda_map) / width)
       print(f'Rooms per width: {rooms_per_width}')
@@ -26,36 +40,58 @@ def generate_zelda_samples(files):
       print(f'Rooms per height: {rooms_per_height}')
       
       print('Generating samples...')
-      
+      rooms_count = 0
       for x in range(rooms_per_width):
         for y in range(rooms_per_height):          
           if (zelda_map[x * width][y * height] == '-'):
             print(f'\tRoom at ({x}, {y}) is empty')
             continue
+          print(f'\tRoom at ({x}, {y}) is not empty')
+
+          # file_path = f'{dir_name}/sample_{file.split("/")[-1].replace(".txt", "")}_{file_count}.txt'
+          # new_file = open(file_path, 'w')
           
-          dir_name = f'src/data/zelda/samples'
-          if not os.path.exists(os.path.dirname(dir_name)):
-            os.mkdir(dir_name)
-            
-          file_path = f'{dir_name}/sample_{file.split("/")[-1].replace(".txt", "")}_{file_count}.txt'
-          new_file = open(file_path, 'w')
+          room = []
           for i in range(width):
             row = []
             for j in range(height):              
               row.append(zelda_map[x * width + i][y * height + j])
-            new_file.write(''.join(row) + '\n')
-          new_file.close()
-          file_count += 1
+            room.append(row)
+          samples.append(room)
+          rooms_count += 1
       
-      print('Generated samples:', file_count)
-      print()
+    print('Generated samples:', rooms_count)
+    samples_count += rooms_count
+    print('Total samples:', samples_count)     
+    print('Removing duplicates...')
+    samples_no_duplicates = []
+    for sample in samples:
+      if sample not in samples_no_duplicates:
+        samples_no_duplicates.append(sample)
+    print('Removed duplicates:', len(samples_no_duplicates))
+    print()
+  
+  print('Total samples:', len(samples_no_duplicates))
+  print('Saving samples...')
+  dir_name = 'src/data/zelda/samples'
+  if not os.path.exists(dir_name):
+    print('\tCreating directory:', dir_name)
+    os.mkdir(dir_name)
+    print('\tDirectory created')
+  for i in range(len(samples_no_duplicates)):
+    json_file = f'{dir_name}/sample_{i}.json'
+    with open(json_file, 'w') as f:
+      json.dump(samples_no_duplicates[i], f)
+  print('Samples saved')
+  print()
+  
 
 ###
 # GENERAL HELP FUNCTIONS
 ###
-def get_vglc_samples(dir_path, game):
+def get_vglc_samples(game_dir, game):
   files = []
-  for file in os.listdir(dir_path):
+  for file in os.listdir(os.path.join(game_dir, 'vglc')):
     if game == 'zelda' and file.endswith('.txt') and file.startswith('tloz'):
       files.append(f'src/data/{game}/vglc/{file}')
     elif game == 'mario' and file.endswith('.txt') and file.startswith('mario'):
@@ -65,9 +101,48 @@ def get_vglc_samples(dir_path, game):
 def generate_samples (files, game):
   if game == 'zelda':
     return generate_zelda_samples(files)
+  if game == 'mario':
+    return generate_mario_samples(files)
+  
+def unify_and_translate_samples(game_dir):
+  print('Loading samples...')
+  samples = []
+  for file in os.listdir(os.path.join(game_dir, 'samples')):
+    with open(os.path.join(game_dir, 'samples', file), 'r') as f:
+      samples.append(json.load(f))
+  print('Total samples:', len(samples))
+  print()
+  
+  game_translations = {}
+  if game == 'zelda':
+    game_translations = json.load(open(os.path.join(game_dir, 'zelda-to-rogue-tiles.json')))
+  if game == 'mario':
+    pass
+  
+  print('Translating samples...')
+  translated_samples = []
+  for sample in samples:
+    translated_sample = []
+    for row in sample:
+      translated_row = []
+      for tile in row:
+        translated_row.append(game_translations[tile])
+      translated_sample.append(translated_row)
+    translated_samples.append(translated_sample)
+  print('Samples translated')
+  print()
+  
+  print('Saving unified samples at json...')
+  json_file = os.path.join(game_dir, 'unified_samples.json')
+  with open(json_file, 'w') as f:
+    json.dump(translated_samples, f)    
+  print('Unified samples saved at json')
+  print()
+  return samples
       
 game = opt.game
-game_vglc_dir = f'src/data/{game}/vglc'    
-
-files = get_vglc_samples(game_vglc_dir, game)
+game_dir = f'src/data/{game}'    
+files = get_vglc_samples(game_dir, game)
 generate_samples(files, game)  
+unify_and_translate_samples(game_dir)
+print('DONE!', 'Database created for', game)
